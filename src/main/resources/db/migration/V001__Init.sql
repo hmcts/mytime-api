77b8d9643d4a1d4441dc123ad03f849086f04e67
StuartBJustice <stuart.bolton@justice.gov.uk>
2023-02-14 12:42:46 +0000

    Revert V001 for build

--- src/main/resources/db/migration/V001__Init.sql
+++ src/main/resources/db/migration/V001__Init.sql
@@ -1,25 +1,13 @@
 SELECT pg_catalog.set_config('search_path', '', false);
-SET
-statement_timeout = 0;
-SET
-lock_timeout = 0;
-SET
-idle_in_transaction_session_timeout = 0;
-SET
-client_encoding = 'UTF8';
-SET
-standard_conforming_strings = on;
-SET
-check_function_bodies = false;
-SET
-xmloption = content;
-SET
-client_min_messages = warning;
-SET
-row_security = off;
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
 
 -- Migration simply to check it gets applied through flyway...
-SELECT COUNT(DISTINCT 'table_name')
-FROM information_schema.columns
-WHERE table_schema = 'public';
-
+SELECT COUNT(DISTINCT 'table_name') FROM information_schema.columns WHERE table_schema = 'public';
