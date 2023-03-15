0f96fdc795061ce6bb011c8f1fdf382cdd38bc47
newmanjustice <stevebeanblue@aol.com>
2023-03-15 15:35:51 +0000

    Update UserControllerTest.java

--- src/functionalTest/java/uk/gov/hmcts/dts/controllers/UserControllerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/dts/controllers/UserControllerTest.java
@@ -2,25 +2,27 @@ package uk.gov.hmcts.dts.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.response.Response;
+import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.dts.controllers.util.FunctionalTestBase;
 import uk.gov.hmcts.dts.mytime.models.UserModel;
 
-import java.time.LocalDateTime;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
-
 @ExtendWith(SpringExtension.class)
 @SuppressWarnings("PMD.TooManyMethods")
 class UserControllerTest extends FunctionalTestBase {
-    private static final String PATH = "/user";
+
+    private static final ThreadLocalRandom random = ThreadLocalRandom.current();
+    private static final String PATH = "/user/6";
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private static final Integer ID = 97;
+    private static final Integer ID = random.nextInt(1999, 2999);
     private static final String FORENAME = "Functional";
     private static final String SURNAME = "Test";
     private static final LocalDateTime DATE_JOINED = LocalDateTime.now();
@@ -44,46 +46,84 @@ class UserControllerTest extends FunctionalTestBase {
 
     @Test
     void shouldSaveNewUser() throws JsonProcessingException {
-        String jsonInput = OBJECT_MAPPER.writeValueAsString(USER_MODEL);
-        var response = doPutRequest(PATH + "/save", jsonInput);
-        assertThat(response.statusCode())
-            .isEqualTo(CREATED.value());
 
+        // create new user
+        var response = saveUser();
+        assertThat(response.statusCode()).isEqualTo(CREATED.value());
+        // check saved user is the same
         UserModel savedUser = response.getBody().as(UserModel.class);
         assertThat(savedUser.getForeName()).isEqualTo(FORENAME);
+
+        // tidy up
+        response = deleteUser();
+        assertThat(response.statusCode()).isEqualTo(OK.value());
     }
 
     @Test
-    void shouldGetUserByID() {
-        var path  = PATH + "/2";
+    void shouldGetUserByID() throws JsonProcessingException {
+
+        // save user to db to retrieve
+        //var response = saveUser();
+        //assertThat(response.statusCode()).isEqualTo(CREATED.value());
+
+        // get saved user by ID
+        var path  = PATH ;
+            //+ "/" + 6;
+            // USER_MODEL.getId();
         var response = doGetRequest(path);
         assertThat(response.statusCode())
             .isEqualTo(OK.value());
 
+        // check returned user object
         UserModel user = response.getBody().as(UserModel.class);
-        assertThat(user.getForeName()).isEqualTo("Ida");
+        assertThat(user.getForeName()).isEqualTo(USER_MODEL.getForeName());
+
+        // tidy up
+        //response = deleteUser();
+        //assertThat(response.statusCode()).isEqualTo(OK.value());
+
     }
 
     @Test
     void shouldUpdateUser() throws JsonProcessingException {
-        UserModel updatedUser = OBJECT_MAPPER.readValue(
-            OBJECT_MAPPER.writeValueAsString(USER_MODEL), UserModel.class
-        );
-        var path = PATH + "/update";
+
+        // save user to db to update
+        var response = saveUser();
+        assertThat(response.statusCode()).isEqualTo(CREATED.value());
+
+        // get returned saved user and update property
+        UserModel updatedUser = response.getBody().as(UserModel.class);
         updatedUser.setForeName("Updated Forename");
+        var path = PATH + "/update";
         var jsonObj = OBJECT_MAPPER.writeValueAsString(updatedUser);
-        var response = doPatchRequest(path, jsonObj);
+        response = doPatchRequest(path, jsonObj);
         var returnedUser = response.getBody().as(UserModel.class);
         assertThat(returnedUser.getForeName()).isEqualTo(updatedUser.getForeName());
+
+        // tidy up
+        response = deleteUser();
+        assertThat(response.statusCode()).isEqualTo(OK.value());
     }
 
     @Test
     void shouldDeleteUser() throws JsonProcessingException {
-        var path = PATH + "/delete";
-        UserModel userToDelete = new UserModel();
-        userToDelete.setId(5);
-        var jsonObj = OBJECT_MAPPER.writeValueAsString(userToDelete);
-        var response = doDeleteRequest(path, jsonObj);
+
+        // save user to db to delete
+        var response = saveUser();
+        assertThat(response.statusCode()).isEqualTo(CREATED.value());
+
+        // tidy up
+        response = deleteUser();
         assertThat(response.statusCode()).isEqualTo(OK.value());
+    }
+
+    private Response saveUser() throws JsonProcessingException {
+        return doPutRequest(PATH + "/save",
+                            OBJECT_MAPPER.writeValueAsString(USER_MODEL));
+    }
+
+    private Response deleteUser() throws JsonProcessingException {
+        return doDeleteRequest(PATH + "/delete",
+                               OBJECT_MAPPER.writeValueAsString(USER_MODEL));
     }
 }
